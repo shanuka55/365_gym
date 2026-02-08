9de02019baf981e610f5d4c95ac09cfc6004af14
shanuka55 <shanukalakshan5555@gmail.com>
2026-02-08 07:01:29 +0400

    Test

--- src/hooks/usePageTracking.ts
+++ src/hooks/usePageTracking.ts
@@ -8,7 +8,9 @@ const usePageTracking = () => {
         window.dataLayer = window.dataLayer || [];
         window.dataLayer.push({
             event: "page_view",
-            page_path: location.pathname,
+            page_location: window.location.href, // full URL
+            page_path: location.pathname,        // / , /about , /contact
+            page_title: document.title,           // browser title
         });
     }, [location.pathname]);
 };
