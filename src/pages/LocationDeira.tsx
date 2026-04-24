6106fbaea6c23404ba2de8e8aee626319b5d6b3a
shanuka55 <shanukalakshan5555@gmail.com>
2026-04-24 14:27:41 +0400

    test

--- src/pages/LocationDeira.tsx
+++ src/pages/LocationDeira.tsx
@@ -335,7 +335,7 @@ const LocationDeira = () => {
                       {plan.offer.map((offer) => (
                         <div
                           key={offer.name}
-                          className={offer.included ? "text-green-600" : "text-muted-foreground line-through opacity-50"}
+                          className={offer.included ? "text-yellow-800" : "text-muted-foreground line-through opacity-80"}
                         >
                           {offer.name}
                         </div>
